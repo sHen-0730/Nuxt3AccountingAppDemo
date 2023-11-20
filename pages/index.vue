57ce983931b31c76fb9a1a83ecffbb76d961f820
sHen <103980830+sHen-0730@users.noreply.github.com>
2023-11-20 05:08:45 +0000

    編集

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="container">
     <NuxtLink to="/demo/test" class="">wakaba</NuxtLink>
+    <NuxtLink to="/demo/testn" class="">natuki</NuxtLink>
+  　<NuxtLink to="/demo/testS" class="">soma</NuxtLink>
   </div>
 </template>
